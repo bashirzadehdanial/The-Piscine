397dc1a7fc25f3bdd0d7274f16af5aa917865c7e
bashirzadehdanial <bashirzadehdanial@gmail.com>
2025-02-07 22:51:24 +0000

    Add functionallity for sorting and formatting date

--- Project-Spaced-Repetition-Tracker/script.js
+++ Project-Spaced-Repetition-Tracker/script.js
@@ -8,35 +8,72 @@ import { getUserIds, getData, addData } from "./storage.js";
 
 function userSelected() {
   let select = document.getElementById("user-drop-down");
-  let resultBox = document.getElementById("result-box");
   let userId = select.value;
   const data = getData(userId);
   displayUserData(userId, data);
 }
 
 function displayUserData(id, data) {
-  let resultBox = document.getElementById("result-box");  
+  let resultBox = document.getElementById("result-box");
+  resultBox.innerHTML = "";
+
   const li = document.createElement("li");
-    resultBox.innerHTML = "";
-    li.textContent = `The agenda for User ${id} is shown, with the revision dates shown as follows:`;
-  if(data === null || data.length < 1) {
+  li.textContent = `The agenda for User ${id} is shown, with the revision dates shown as follows:`;
+
+  if (data === null || data.length < 1) {
     li.textContent = `The User ${id} is empty!`;
     resultBox.append(li);
     return;
   }
+
+  sortRevisionDate(data);
+
+  const subUl = document.createElement("ul");
+  data.forEach(function (item) {
+    const subLi = document.createElement("li");
+    subLi.textContent = item.topic + ", " + item.date;
+    subUl.append(subLi);
+  });
+
+  li.append(subUl);
+  resultBox.append(li);
 }
 
+function sortRevisionDate(data) {
+  return data.sort((a, b) => {
+    let dateA = a.date.replace(/(st|nd|rd|th)/, "");
+    dateA = new Date(dateA);
+    let dateB = b.date.replace(/(st|nd|rd|th)/, "");
+    dateB = new Date(dateB);
+
+    return dateA - dateB;
+  });
+}
+
+function formatDate(date) {
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+
+  let suffix = "th";
+  if (day === 1 || day === 21 || day === 31) suffix = "st";
+  else if (day === 2 || day === 22) suffix = "nd";
+  else if (day === 3 || day === 23) suffix = "rd";
+
+  return `${day}${suffix} ${month} ${year}`;
+}
 
 window.onload = function () {
   const users = getUserIds();
   const data = getData(users[0]);
-  let myForm = document.getElementById("agenda-form");
   let selectElement = document.getElementById("user-drop-down");
-  users.forEach(function(value) {
+
+  users.forEach(function (value) {
     let option = document.createElement("option");
     option.innerHTML = "User " + value;
     option.value = value;
     selectElement.append(option);
   });
+
   selectElement.addEventListener("change", userSelected);
 };
